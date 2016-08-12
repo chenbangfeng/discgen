25ca0079148fca66dfa0a200a87cf14d2f3c9a5c
Tom White <tom@sixdozen.com>
2016-08-13 03:18:39 +1200

    Apply discriminative regularization to mlp output.

--- experiments/train_celeba_vae.py
+++ experiments/train_celeba_vae.py
@@ -262,6 +262,7 @@ def create_training_computation_graphs(discriminative_regularization):
             classifier_model = Model(load(src).algorithm.cost)
         selector = Selector(classifier_model.top_bricks)
         classifier_convnet, = selector.select('/convnet').bricks
+        classifier_mlp, = selector.select('/mlp').bricks
     random_brick = Random()
 
     # Initialize conditional variances
@@ -278,6 +279,12 @@ def create_training_computation_graphs(discriminative_regularization):
                 name='{}_log_sigma'.format(layer.name))
             add_role(log_sigma, PARAMETER)
             variance_parameters.append(log_sigma)
+        # include mlp
+        log_sigma = shared_floatx(
+            numpy.zeros([classifier_mlp.output_dim]),
+            name='{}_log_sigma'.format("MLP"))
+        add_role(log_sigma, PARAMETER)
+        variance_parameters.append(log_sigma)
 
     # Computation graph creation is encapsulated within this function in order
     # to allow selecting which parts of the graph will use batch statistics for
@@ -313,24 +320,31 @@ def create_training_computation_graphs(discriminative_regularization):
         if discriminative_regularization:
             # Propagate both the input and the reconstruction through the
             # classifier
-            acts_cg = ComputationGraph([classifier_convnet.apply(x)])
+            acts_cg = ComputationGraph([classifier_mlp.apply(classifier_convnet.apply(x).flatten(ndim=2))])
             acts_hat_cg = ComputationGraph(
-                [classifier_convnet.apply(mu_theta)])
+                [classifier_mlp.apply(classifier_convnet.apply(mu_theta).flatten(ndim=2))])
 
             # Retrieve activations of interest and compute discriminative
             # regularization reconstruction terms
-            for layer, log_sigma in zip(classifier_convnet.layers[4::6],
+            for layer, log_sigma in zip(classifier_convnet.layers[4::6] + [classifier_mlp],
                                         variance_parameters[1:]):
                 variable_filter = VariableFilter(roles=[OUTPUT],
                                                  bricks=[layer])
                 d, = variable_filter(acts_cg)
                 d_hat, = variable_filter(acts_hat_cg)
-                log_sigma = log_sigma.dimshuffle('x', 0, 1, 2)
+
+                # TODO: this conditional could be less brittle
+                if "mlp" in layer.name.lower():
+                    log_sigma = log_sigma.dimshuffle('x', 0)
+                    sumaxis = [1]
+                else:
+                    log_sigma = log_sigma.dimshuffle('x', 0, 1, 2)
+                    sumaxis = [1, 2, 3]
 
                 total_reconstruction_term += -0.5 * (
                     tensor.log(2 * pi) + 2 * log_sigma +
                     (d - d_hat) ** 2 / tensor.exp(2 * log_sigma)
-                ).sum(axis=[1, 2, 3])
+                ).sum(axis=sumaxis)
 
         cost = (kl_term - total_reconstruction_term).mean()
 
