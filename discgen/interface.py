6125969c3e6f007612a624dce688f3976069bea2
Tom White <tom@sixdozen.com>
2016-10-17 13:56:32 +1300

    interaface: now caches compiled encoder/decoder functions

--- discgen/interface.py
+++ discgen/interface.py
@@ -27,8 +27,28 @@ def get_image_encoder_function(model):
         computation_graph.inputs, computation_graph.outputs)
     return encoder_function
 
+def get_decoder_function(model):
+    selector = Selector(model.top_bricks)
+    decoder_mlp, = selector.select('/decoder_mlp').bricks
+    decoder_convnet, = selector.select('/decoder_convnet').bricks
+
+    print('Building computation graph...')
+    z = tensor.matrix()
+    mu_theta = decoder_convnet.apply(
+        decoder_mlp.apply(z).reshape(
+            (-1,) + decoder_convnet.get_dim('input_')))
+    computation_graph = ComputationGraph([z, mu_theta])
+
+    print('Compiling sampling function...')
+    decoder_function = theano.function(
+        computation_graph.inputs, computation_graph.outputs)
+
+    return decoder_function
+
 class DiscGenModel:
     def __init__(self, filename=None, model=None):
+        self.encoder_function = None
+        self.sampling_function = None
         if model is not None:
             self.model = model
         else:
@@ -40,9 +60,10 @@ class DiscGenModel:
                     self.model = Model(load(src).algorithm.cost)
 
     def encode_images(self, images):
-        encoder_function = get_image_encoder_function(self.model)
+        if self.encoder_function is None:
+            self.encoder_function = get_image_encoder_function(self.model)
         print('Encoding...')
-        examples, latents = encoder_function(images)
+        examples, latents = self.encoder_function(images)
         return latents
 
     def get_zdim(self):
@@ -51,22 +72,10 @@ class DiscGenModel:
         return decoder_mlp.input_dim
 
     def sample_at(self, z):
-        selector = Selector(self.model.top_bricks)
-        decoder_mlp, = selector.select('/decoder_mlp').bricks
-        decoder_convnet, = selector.select('/decoder_convnet').bricks
-
-        print('Building computation graph...')
-        sz = shared_floatx(z)
-        mu_theta = decoder_convnet.apply(
-            decoder_mlp.apply(sz).reshape(
-                (-1,) + decoder_convnet.get_dim('input_')))
-        computation_graph = ComputationGraph([mu_theta])
-
-        print('Compiling sampling function...')
-        sampling_function = theano.function(
-            computation_graph.inputs, computation_graph.outputs[0])
-
+        if self.sampling_function is None:
+            self.sampling_function = get_decoder_function(self.model)
+        z_float = theano._asarray(z, dtype=theano.config.floatX)
         print('Sampling...')
-        samples = sampling_function()
+        latents, samples = self.sampling_function(z_float)
+        print(samples.shape)
         return samples
-
